0289140cace02af141e07d7d7c47c08e699cd122
Ans <anasashfaq00@gmail.com>
2024-07-30 14:39:19 +0500

    Added a chart in home page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Layout } from "../components/base/layout";
+import { BarChartComponent } from "../components/custom/charts";
+import { CUSTOMERS_DATA } from "../data/customers";
 
 export function HomePage()
 {
     return (
         <Layout>
-            Home
+            <BarChartComponent chartData={CUSTOMERS_DATA} />
         </Layout>
     )
 }
