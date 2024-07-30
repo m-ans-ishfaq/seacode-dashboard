a11bffb696a43a9c1198babdccfb288eb9f5773d
Ans <anasashfaq00@gmail.com>
2024-07-30 15:19:27 +0500

    Made header show user when logged in and logged out

--- src/components/base/header.jsx
+++ src/components/base/header.jsx
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { useUsers } from "../../hooks/useUsers";
 
 export function Header({ title }) {
+
+    const { logOut, loggedInUser } = useUsers();
+
     return (
         <header className="px-8 py-4 h-16 border-b border-b-slate-800 flex justify-between items-center">
             <h2 className="text-neutral-300 font-medium text-lg">
                 {title}
             </h2>
             <div className="flex items-center gap-6">
-                <Button variant="ghost" className="text-base">
-                    <Link to="/sign-up">
-                        Sign Up
-                    </Link>
-                </Button>
-                <Button variant="ghost" className="text-base">
-                    <Link to="/log-in">
-                        Log In
-                    </Link>
-                </Button>
+                {loggedInUser ? (
+                    <>
+                        <h4>{loggedInUser.username}</h4>
+                        <Button variant="destructive" className="text-base" onClick={() => logOut()}>
+                            Log Out
+                        </Button>
+                    </>
+                ) : (
+                    <>
+                        <Button variant="ghost" className="text-base">
+                            <Link to="/sign-up">
+                                Sign Up
+                            </Link>
+                        </Button>
+                        <Button variant="ghost" className="text-base">
+                            <Link to="/log-in">
+                                Log In
+                            </Link>
+                        </Button>
+                    </>
+                )}
             </div>
         </header>
     )
