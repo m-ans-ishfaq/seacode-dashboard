7a4d4b336e60b866311b9fbc82954fa0f0d79403
Ans <anasashfaq00@gmail.com>
2024-07-30 15:27:32 +0500

    Added hidden admin page

--- src/routes/users.jsx
+++ src/routes/users.jsx
@@ -1,10 +1,21 @@
 import { Layout } from "../components/base/layout";
+import { useUsers } from "../hooks/useUsers";
 
 export function Users()
 {
+    const { users, admin } = useUsers();
+
+    if (!admin) return (
+        <Layout title="Unauthorized">
+            <div className="w-full h-full flex items-center justify-center">
+                You don't have access to this page
+            </div>
+        </Layout>
+    );
+
     return (
         <Layout title="Users">
-            Users
+            You have users
         </Layout>
     )
 }
